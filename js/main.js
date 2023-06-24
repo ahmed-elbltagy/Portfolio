a02a9f64c904e20bab80aaba913211a04e861f66
ahmedelbltagy <ahmedelbltagy100@gmail.com>
2023-06-25 00:19:41 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,66 +1,25 @@
-/*
- ** Countdown Timer
- ** Video URL: https://www.youtube.com/watch?v=eFsiOTJrrE8
- */
-
-// The End Of The Year Date
-// 1000 milliseconds = 1 Second
-
-// let countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
-// console.log(countDownDate);
-
-// let counter = setInterval(() => {
-  // Get Date Now
-  // let dateNow = new Date().getTime();
-
-  // Find The Date Difference Between Now And Countdown Date
-  // let dateDiff = countDownDate - dateNow;
-
-  // Get Time Units
-  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
-  // let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
-  // let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  // let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
-  // let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
-
-  // document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
-  // document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
-  // document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
-  // document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
-
-//   if (dateDiff < 0) {
-//     clearInterval(counter);
-//   }
-// }, 1000);
 
 
-/*
- ** Increase Numbers On Scrolling
- ** Video URL: https://youtu.be/PLsUdgLnzgQ
- */
+let counter =setInterval( function () { 
+  let date1 = new Date("2021-03-23, 23:59:59");
+  let date2 = new Date();
+  let dateDiff = date2 - date1;
+  let years = Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 365.25));
+  let months = Math.floor((dateDiff / (1000 * 60 * 60 * 24 * 30.44)) % 12);
+  let days = Math.floor((dateDiff / (1000 * 60 * 60 * 24 )) % 30.44);
+  let hours = Math.floor( ( dateDiff / ( 1000 * 60 * 60 ) ) % 24 );
+  let minutes = Math.floor((dateDiff / (1000 * 60)) % 60);
+  let seconds = Math.floor( ( dateDiff / ( 1000 ) ) % 60 );
+  document.querySelector('.years').innerHTML=years
+  document.querySelector('.months').innerHTML=months
+  document.querySelector('.days').innerHTML=days
+  document.querySelector('.hours').innerHTML=hours
+  document.querySelector('.minutes').innerHTML=minutes
+  document.querySelector('.seconds').innerHTML=seconds
+}, 500 )
 
-// let progressSpans = document.querySelectorAll(".the-progress span");
-// let section = document.querySelector(".our-skills");
 
-// let nums = document.querySelectorAll(".stats .number");
-// let statsSection = document.querySelector(".stats");
-// let started = false; // Function Started ? No
 
-// window.onscroll = function () {
-
-  // if (window.scrollY >= section.offsetTop - 250) {
-  //   progressSpans.forEach((span) => {
-  //     span.style.width = span.dataset.width;
-  //   });
-  // }
-  // Stats Increase Number
-//   if (window.scrollY >= statsSection.offsetTop) {
-//     if (!started) {
-//       nums.forEach((num) => startCount(num));
-//     }
-//     started = true;
-//   }
-// };
 
 // function startCount(el) {
 //   let goal = el.dataset.goal;
@@ -72,18 +31,16 @@
 //   }, 2000 / goal);
 // }
 
-  // Skills Animate Width
 let section = document.querySelector("#skills");
 let progressSpan = document.querySelectorAll(".prog span");
 window.addEventListener("scroll", function() {
-  if (window.scrollY >= section.offsetTop -10) {
+  if (window.scrollY >= section.offsetTop -270) {
     progressSpan.forEach( ( span ) =>{
-      span.style.width = span.dataset.width
-  })
+      span.style.width = span.dataset.width})
   }
 });
 
-// scroll to Top button
+/////////////////////////////
 let mybutton = document.querySelector( ".btn" );
 window.onscroll = function(){
   if ( window.scrollY >= 650 )
